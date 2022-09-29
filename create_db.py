1957c2afebdca7ccc7e180332f02988518ea5c32
punith101 <narayanaswamy.p@husky.neu.edu>
2022-09-30 03:15:15 +0400

    ayh

--- create_db.py
+++ create_db.py
@@ -2,7 +2,7 @@ import sqlite3 as sql
 
 # connect to SQLite
 con = sql.connect(
-    "postgres://acgqiwenxquwam:8dbc253e737f6e1c7441bfe71a7c080d8c354f4a9c9171528e4c0cf98f91f214@ec2-3-229-252-6.compute-1.amazonaws.com:5432/d4esd7evbkcdr0")
+    "postgres://skswsshtsdgrss:8e33867b45d5bf6bfab33a35191654f8a1f1a4326f23bae32f079fb81a709fec@ec2-54-91-223-99.compute-1.amazonaws.com:5432/d8sts52g57acmu")
 
 # Create a Connection
 cur = con.cursor()
