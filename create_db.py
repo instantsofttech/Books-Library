0d348eaf0aae5f30baf9005c838d0fe6fb82a284
punith101 <narayanaswamy.p@husky.neu.edu>
2022-09-30 03:07:24 +0400

    aj

--- create_db.py
+++ create_db.py
@@ -2,7 +2,7 @@ import sqlite3 as sql
 
 # connect to SQLite
 con = sql.connect(
-    'postgres://acgqiwenxquwam:8dbc253e737f6e1c7441bfe71a7c080d8c354f4a9c9171528e4c0cf98f91f214@ec2-3-229-252-6.compute-1.amazonaws.com:5432/d4esd7evbkcdr0')
+    'db_web.db')
 
 # Create a Connection
 cur = con.cursor()
